7df48baba413c8f6236a79eb4099e8a8f8588c08
milesgwood <miles.gwood@gmail.com>
2018-04-19 11:28:27 -0400

    responsive ledger store page

---
+++ scripts.js
@@ -0,0 +1,28 @@
+(function($){
+
+    $( document ).ready(function() {
+        init();
+    });
+
+    function init() {
+        smooth_anchor_link_jumping();
+    }
+
+
+    /*
+    Smooth scroll functionality for anchor links (animates the scroll
+    rather than a sudden jump in the page)
+    */
+    function smooth_anchor_link_jumping() {
+
+        $('.js-anchor-link').click(function(e){
+            e.preventDefault();
+            var target = $($(this).attr('href'));
+            if(target.length){
+                var scrollTo = target.offset().top;
+                $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
+            }
+        });
+    }
+})(jQuery);
+
